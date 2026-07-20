61bad68da9c568557341969a41adfe5d2bc2d56b
ImadBen96 <ibenomar536@gmail.com>
2026-07-20 19:42:20 +0200

    change service and vercel files

--- built/server.js
+++ built/server.js
@@ -15,23 +15,19 @@ var path_1 = __importDefault(require("path"));
 (0, database_config_1.dbConnect)();
 var app = (0, express_1.default)();
 app.use(express_1.default.json());
-//app.get('/*', express.static('../../frontend/dist/frontend/browser'));
-//app.use(express.static(path.resolve('../../frontend/dist/frontend/browser')));
-// Catch-all route to serve index.html for Angular routes
-//app.use(express.static(path.resolve('../../frontend/dist/frontend/browser')));
-// Serve static files from the Angular app
-app.use(express_1.default.static(path_1.default.join(__dirname, '../../frontend/dist/frontend/browser')));
+app.use(express_1.default.static(path_1.default.join(__dirname, '../dist/frontend/browser')));
 app.use((0, cors_1.default)({
     credentials: true,
-    origin: ["http://localhost:4200"]
+    origin: ["https://food-store-ver.vercel.app"]
 }));
 app.use("/api/foods", food_router_1.default);
 app.use("/api/users", user_router_1.default);
 app.use("/api/orders", order_router_1.default);
 app.get('*', function (req, res) {
-    res.sendFile(path_1.default.join(__dirname, '../../frontend/dist/frontend/browser/index.html'));
+    res.sendFile(path_1.default.join(__dirname, '../dist/frontend/browser/index.html'));
 });
 var port = 5000;
 app.listen(port, function () {
     console.log("WebSite Served On http://localhost:" + port);
 });
+exports.default = app;
